47707c181f0f95c16a7a9d70ac4b9f70fb638cb1
CalebObsCode <Sommskycal345@gmail.com>
2022-08-14 19:54:29 -0400

    Adding popup to prompt for password name

--- Apps.py
+++ Apps.py
@@ -1,6 +1,7 @@
 import tkinter
 import random
 import string
+from tkinter.ttk import Label
 import pyperclip
 from PIL import ImageTk, Image
 
@@ -44,18 +45,33 @@ def clear_password():
     messageboxes.place(x=305, y=220)
 
 
-def save_password():
+
+
+# Function used to save the password to a file
+"""
+Function will prompt the user to enter a name for the password and then save the password to a file
+txt will have the following format:
+password name    password
+"""
+def save_password( name ):
+    # Key value pair for the password name and password
     Password_Array = []
     if getPassword.get() != "":
-        Password_Array.append(getPassword.get())
-        with open('Generated Passwords.txt', 'a') as f:
-            for item in Password_Array:
-                f.write("%s\n" % item)
-        messageboxes = tkinter.Label(master, text='Password saved', font='Ubuntu 12', width=15)
-        messageboxes.place(x=305, y=220)
+        with open('GeneratedPasswords.txt', 'a') as file:
+            # Create a key value pair for the password name and password
+            file.write("Password Name: " + name + "\t\t" + "Saved Password: "  +getPassword.get() + "\n")
+            # Save file to array
+            Password_Array.append(name + " " + getPassword.get())
+            # Clear the password entry field
+            getPassword.set("")
+            # Display a message to the user that the password has been saved
+            messageboxes = tkinter.Label(master, text='Password saved', font='Ubuntu 12', width=15)
+            messageboxes.place(x=305, y=220)
     else:
         messageboxes = tkinter.Label(master, text='Nothing to save', font='Ubuntu 12', width=15)
         messageboxes.place(x=305, y=220)
+
+
 
 
 
@@ -114,8 +130,31 @@ clear_button = tkinter.Button(master, text='Clear', command=clear_password)
 clear_button.pack(padx=50,pady=5,side=tkinter.RIGHT)
 
 
+def open_popup():
+    # popup will have an input field for the user to enter a name for the password
+    popup = tkinter.Toplevel(master)
+    popup.geometry("350x220")
+    popup.title("Save Password")
+    Label(popup, text="Enter a name for the password:").pack()
+    entry = tkinter.Entry(popup)
+    entry.pack()
+    saveButton = tkinter.Button(popup, text="Save", command=lambda: save_password(entry.get()))
+    saveButton.pack()
+    cancelButton = tkinter.Button(popup, text="Cancel", command=popup.destroy)
+    cancelButton.pack()
+
+    # Show "Saved Password" message when password is saved
+    messageboxes = tkinter.Label(master, text='Password saved', font='Ubuntu 12', width=15)
+    messageboxes.place(x=305, y=220)
+    
+
+
+
+
 # Button to call a Save Password function that will save the password to a text file on the user computer
-save_button = tkinter.Button(master, text='Save', command=save_password)
+# if a user clicks the save button, create another popup window that will ask the user to enter a name for that password
+# Onclick popup window will ask the user to enter a name for the password and then save the password to a text file
+save_button = tkinter.Button(master, text='Save', command=open_popup)
 save_button.pack(padx=50,pady=5,side=tkinter.RIGHT)
 
 
